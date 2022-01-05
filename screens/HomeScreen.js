c58db04faf37237c26fab1c1bf3528cb589a83f9
abayram <38274063+abdullahbayram@users.noreply.github.com>
2022-01-05 10:23:05 +0100

    fix: expo-status-bar not found

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
     return (
         <View style={styles.container}>
             <Text>Storybook Sample!!!</Text>
-            <StatusBar style="auto" />
         </View>
     );
 }
